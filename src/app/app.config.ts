522b0604c1d58a9050db02547963209d796b7796
Armando Navarro <armando.developer@outlook.com>
2025-12-02 17:01:33 -0800

    add app config, global styles/variables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
-import { provideRouter } from '@angular/router'
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core'
+import { provideRouter, withComponentInputBinding } from '@angular/router'
+import { provideHttpClient } from '@angular/common/http'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { MatNativeDateModule } from '@angular/material/core'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
+		provideRouter(routes, withComponentInputBinding()),
+		provideHttpClient(),
 		provideAnimationsAsync(),
+		importProvidersFrom(MatNativeDateModule),
 	]
 }
