2e89927eaacc910158cdd44a6b6fc5206f44d4de
Armando Navarro <armando.developer@outlook.com>
2025-12-10 04:37:50 -0800

    fix page header bar hiding on mobile
    Swiping down to scroll to top of page no
    longer causes header bar to hide.

--- src/app/services/scroll.service.ts
+++ src/app/services/scroll.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core'
-import { distinctUntilChanged, fromEvent, map, pairwise, throttleTime } from 'rxjs'
+import { distinctUntilChanged, filter, fromEvent, map, pairwise, throttleTime } from 'rxjs'
 
 export type ScrollDirection = 'up'|'down'
 
 @Injectable({ providedIn: 'root' })
 export class ScrollService {
-	scrollDirection$ = fromEvent(window, 'scroll').pipe(
+	readonly scrollDirection$ = fromEvent(window, 'scroll').pipe(
 		throttleTime(50),
 		map(() => window.scrollY),
+		filter(scrollY => scrollY >= 0),
 		pairwise(),
 		map<[number, number], ScrollDirection>(([ previousY, currentY ]) =>
 			currentY > previousY ? 'down' : 'up'
