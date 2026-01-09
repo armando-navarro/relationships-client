da886094f094ffdd234ebce58d23db88c6f01d07
Armando Navarro <armando.developer@outlook.com>
2026-01-08 19:18:34 -0800

    add GitHub logo to welcome page
    - Fixed incorrect icon styles
    - Icon styles support <a> elements and <img> icons

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,19 +1,25 @@
-import { Component, signal } from '@angular/core'
+import { Component, inject, signal } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { toSignal } from '@angular/core/rxjs-interop'
 
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 import { PageHeaderBarComponent } from '../../components/page-header-bar/page-header-bar.component'
+import { ResponsiveUiService } from '../../services/responsive-ui.service'
 
 @Component({
 	selector: 'app-welcome',
 	standalone: true,
-	imports: [MatButtonModule, MatIconModule, PageHeaderBarComponent, RouterLink],
+	imports: [MatButtonModule, MatIconModule, MatTooltipModule, PageHeaderBarComponent, RouterLink],
 	templateUrl: './welcome.component.html',
 	styleUrl: './welcome.component.scss'
 })
 export class WelcomeComponent {
+	private readonly responsivUiService = inject(ResponsiveUiService)
+
+	readonly isSmallViewport = toSignal(this.responsivUiService.isSmallViewport$)
 	isTableOfContentsOpen = signal(false)
 
 	onTableOfContentsClick(): void {
