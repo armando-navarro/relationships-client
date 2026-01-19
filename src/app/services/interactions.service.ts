992d02de30224381fdd058bf5205986b4efec52a
Armando Navarro <armando.developer@outlook.com>
2026-01-18 21:34:23 -0800

    tweak interaction group titles for readabiliity
    - Instead of '4 days/weeks/months/years ago', specific
      day/week/month/year text is displayed for dates earlier
      than a few days/weeks/months/years ago (depending on
      the time unit set for grouping).
    - Date comparisons are made with the proper dates, instead
      of with the start of the given time unit, resulting in
      more accurate grouping.

--- src/app/services/interactions.service.ts
+++ src/app/services/interactions.service.ts
@@ -67,40 +67,82 @@ export class InteractionsService {
 		)
 	}
 
-	/** Expects an array of interactions sorted descending by date. */
-	groupBy(sortedInteractions: Interaction[], groupBy: TimeUnit, targetInteraction: Interaction): InteractionGroupingResult {
+	/** Expects an array of interactions sorted descending by date.
+	 * Groups interactions by the specified number of time units passed since the interaction occurred.
+	 * @returns interactions grouped by time unit and the location of the interaction to highlight */
+	groupBy(sortedInteractions: Interaction[], groupBy: TimeUnit, highlightInteraction: Interaction): InteractionGroupingResult {
 		const groupedInteractions: InteractionGroup[] = []
 		let groupKey = ''
 		let indexInGroup = -1
 
 		sortedInteractions.forEach(interaction => {
-			const startOfTargetTimeUnit = DateTime.fromJSDate(interaction.date!).startOf(groupBy, { useLocaleWeeks: true })
-			const startOfCurrentTimeUnit = DateTime.now().startOf(groupBy, { useLocaleWeeks: true })
-			const timeUnitsAgo = startOfCurrentTimeUnit.diff(startOfTargetTimeUnit, `${groupBy}s`).as(`${groupBy}s`)
-			const timeAgoText = startOfTargetTimeUnit.toRelativeCalendar({ base: startOfCurrentTimeUnit, unit: `${groupBy}s` }) || ''
+			// each interaction occurred before or on the same date as the previous interaction in the loop
+			const interactionDate = DateTime.fromJSDate(interaction.date!)
+			// number of days/weeks/months/years between interaction date and now
+			const timeUnitsAgo = DateTime.now().diff(interactionDate, `${groupBy}s`).as(`${groupBy}s`)
+			// text like "Today", "Last week", "3 months ago"
+			const timeAgoText = this.getTimeAgoText(interactionDate, groupBy)
+			// since interactions are sorted, we only need to check the last group added to see if this interaction belongs there
+			let lastGroup = groupedInteractions[groupedInteractions.length - 1]
 
-			const lastGroup = groupedInteractions[groupedInteractions.length - 1]
-			if (!lastGroup || lastGroup.timeUnitsAgo !== timeUnitsAgo) {
-				groupedInteractions.push({
-					groupedBy: groupBy,
-					timeUnitsAgo,
-					timeAgoText: timeAgoText.replace(/\b\w/g, (char) => char.toUpperCase()),
-					interactions: [interaction]
-				})
-				if (interaction._id === targetInteraction._id) {
-					groupKey = groupedInteractions[groupedInteractions.length-1].timeAgoText
-					indexInGroup = 0
-				}
-			}
-			else {
+			// create group for this time unit
+			if (!lastGroup || lastGroup.timeAgoText !== timeAgoText) {
+				groupedInteractions.push({ groupedBy: groupBy, timeUnitsAgo, timeAgoText, interactions: [interaction] })
+			} else { // group already exists for this time unit
 				lastGroup.interactions.push(interaction)
-				if (interaction._id === targetInteraction._id) {
-					groupKey = lastGroup.timeAgoText
-					indexInGroup = lastGroup.interactions.length - 1
-				}
+			}
+
+			// check if this is the interaction to highlight
+			lastGroup = groupedInteractions[groupedInteractions.length - 1]
+			if (interaction._id === highlightInteraction._id) {
+				groupKey = timeAgoText
+				indexInGroup = lastGroup.interactions.length - 1
 			}
 		})
 		return { groups: groupedInteractions, groupKey, indexInGroup }
+	}
+
+	/** @returns A string describing the time elapsed since the given date in the specified time unit.
+	 * E.g. "Today", "Yesterday", "Wednesday", "Last week", "2 months ago", "March 2020", "2021", etc. */
+	private getTimeAgoText(pastDate: DateTime, timeUnit: TimeUnit): string {
+		const now = DateTime.now()
+		const isBeforeCurrentTimeUnit = !pastDate.hasSame(now, timeUnit)
+		const isBeforeLastTimeUnit = isBeforeCurrentTimeUnit && !pastDate.hasSame(now.minus({ [`${timeUnit}s`]: 1 }), timeUnit)
+		const isBeforeLastTwoTimeUnits = isBeforeLastTimeUnit && !pastDate.hasSame(now.minus({ [`${timeUnit}s`]: 2 }), timeUnit)
+
+		const isOverAWeekAgo = pastDate.diffNow('weeks').toObject().weeks! <= -1
+		const isOverAYearAgo = pastDate.diffNow('years').toObject().years! <= -1
+
+		// use specific year for all previous years
+		if (isBeforeCurrentTimeUnit && timeUnit === 'year') {
+			return pastDate.toFormat('yyyy')
+		}
+
+		// use specific month name for dates 2 months ago or earlier, w/year as needed
+		else if (isBeforeLastTimeUnit && timeUnit === 'month') {
+			let format = 'LLLL'
+			if (isOverAYearAgo) format += ' yyyy'
+			return pastDate.toFormat(format)
+		}
+
+		// use specific day name or "week of" for dates 3 days/weeks ago or earlier, w/year as needed
+		else if (isBeforeLastTwoTimeUnits) {
+			if (timeUnit === 'day') {
+				let format = 'cccc'
+				if (isOverAWeekAgo) format += ', LLLL d'
+				if (isOverAYearAgo) format = 'LLLL d, yyyy'
+				return pastDate.toFormat(format)
+			}
+			if (timeUnit === 'week') {
+				let format = 'LLL d'
+				if (isOverAYearAgo) format += ', yyyy'
+				const beginningOfWeek = pastDate.startOf(timeUnit, { useLocaleWeeks: true })
+				return `Week of ${beginningOfWeek.toFormat(format)}`
+			}
+		}
+		// use relative calendar text for everything else (e.g. "Today", "Yesterday", "Last week", etc.)
+		const relativeCalendarText = pastDate.toRelativeCalendar({ base: now, unit: `${timeUnit}s` }) ?? ''
+		return relativeCalendarText.replace(/\b\w/g, char => char.toUpperCase())
 	}
 
 	sortInteractionsDesc(a: Interaction, b: Interaction): number {
